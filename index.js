84faca610c34344f6b254f73c10b19237598784e
RebeccaHA <rebeccahunt034@gmail.com>
2020-07-13 23:52:29 +0100

    Done.

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, iterate) {
+      const newCollection = Array.isArray(collection)? collection.slice(): Object.values(collection)
 
+      for(let i = 0; i < newCollection.length; i++){
+        iterate(newCollection[i])
+      }
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, cb) {
+      if(!collection instanceof Array){
+        collection = Object.values(collection)
+      }
+      const newArr = []
+      for(let i = 0; i < collection.length; i++){
+        newArr.push(cb(collection[i]))
+      }
+      return newArr
     },
 
     reduce: function() {
